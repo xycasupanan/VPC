70c9637daaed65ba959f5f37e050288764ceac7f
Cris_Xyrus Casupanan <cris.xyrus@gmail.com>
2023-07-06 19:32:09 +0800

    project_version-1

--- nat.tf
+++ nat.tf
@@ -1,47 +1,47 @@
 
 #data
 
-data "terraform_remote_state" "xdata" {
-    backend = "local"
+#data "terraform_remote_state" "xdata" {
+#    backend = "local"
 
-    config = {
-        path = "/root/terraform/DEMO/demo1/terraform.tfstate"
-    }
-}
+#    config = {
+#        path = "/root/terraform/DEMO/demo1/terraform.tfstate"
+#    }
+#}
 
 #Nat GW
 resource "aws_eip" "Nat-a" {
-    vpc = "true"
+#    vpc = "true"
 }
 
 resource "aws_eip" "Nat-b" {
-    vpc = "true"
+#    vpc = "true"
 }
 
 resource "aws_nat_gateway" "XY-nat-gateway-a" {
     allocation_id = aws_eip.Nat-a.id
-    subnet_id = data.terraform_remote_state.xdata.outputs.subnet_id
-    depends_on = [
-        data.terraform_remote_state.xdata.outputs.internet_gateway_id
-    ]
+    subnet_id = aws_subnet.XY-private-a.id
+#    depends_on = [
+#        data.terraform_remote_state.xdata.outputs.internet_gateway_id
+#    ]
 
 }
 
 resource "aws_nat_gateway" "XY-nat-gateway-b" {
     allocation_id = aws_eip.Nat-b.id
-    subnet_id = data.terraform_remote_state.xdata.outputs.subnet_id
-    depends_on = [
-        data.terraform_remote_state.xdata.outputs.nternet_gateway_id
-    ]
+    subnet_id = aws_subnet.XY-private-b.id
+#    depends_on = [
+#        data.terraform_remote_state.xdata.outputs.nternet_gateway_id
+#    ]
 
 }
 
 #VPC setup for NAT
 resource "aws_route_table" "XY-nat-route-a" {
-    vpc_id = data.terraform_remote_state.xdata.outputs.vpc_id
+    vpc_id = aws_vpc.XY-VPC.id
     route  {
         cidr_block = "0.0.0.0/0"
-        aws_nat_gateway_id = aws_nat_gateway.XY-nat-gateway-a.id
+        nat_gateway_id = aws_nat_gateway.XY-nat-gateway-a.id
     }
     tags = {
          Name = "XY-nat-gateway-a"
@@ -49,10 +49,10 @@ resource "aws_route_table" "XY-nat-route-a" {
 }
 
 resource "aws_route_table" "XY-nat-route-b" {
-    vpc_id = data.terraform_remote_state.xdata.outputs.vpc_id
+    vpc_id = aws_vpc.XY-VPC.id
     route  {
         cidr_block = "0.0.0.0/0"
-        aws_nat_gateway_id = aws_nat_gateway.XY-nat-gateway-b.id
+        nat_gateway_id = aws_nat_gateway.XY-nat-gateway-b.id
     }
     tags = {
          Name = "XY-nat-gateway-b"
@@ -60,11 +60,11 @@ resource "aws_route_table" "XY-nat-route-b" {
 }
 
 resource "aws_route_table_association" "XY-route-association-a" {
-    subnet_id = data.terraform_remote_state.xdata.outputs.subnet_id
-    route_table_id = data.terraform_remote_state.xdata.outputs.route_table_id
+    subnet_id = aws_subnet.XY-private-a.id
+    route_table_id = aws_route_table.XY-public-route.id
 }
 
 resource "aws_route_table_association" "XY-route-association-b" {
-    subnet_id = data.terraform_remote_state.xdata.outputs.subnet_id
-    route_table_id = data.terraform_remote_state.xdata.outputs.route_table_id
+    subnet_id = aws_subnet.XY-private-b.id
+    route_table_id = aws_route_table.XY-public-route.id
 }
