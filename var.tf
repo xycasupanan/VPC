b8fd1c1233d6be7a955635ab6b14775c45d1bc28
Cris_Xyrus Casupanan <cris.xyrus@gmail.com>
2023-07-08 18:36:39 +0800

    Project V3

---
+++ var.tf
@@ -0,0 +1,14 @@
+
+variable "AMI" {
+    default = "ami-0f74c08b8b5effa56"
+}
+
+variable "INSTANCE_TYPE" {
+    default = "t2.micro"
+
+}
+
+variable "REGION" {
+    deafault = "ap-southeast-1"
+
+}
